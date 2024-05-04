9b4b858f1d21a1147b908fd61a0b9aa91b468cc2
ViVeK-KuMaR3 <v.kumar7643010@gmail.com>
2024-05-04 10:46:34 +0530

    Initial commit

---
+++ init_setup.sh
@@ -0,0 +1,9 @@
+echo [$(date)]= "START"
+echo [$(date)]= "Creating conda env with python version 3.9"
+conda create -n --prefixt ./env python=3.9 -y
+
+
+echo [$(date)]= "conda environment created"
+echo []
+echo [$(date)]= "START"
+echo []
