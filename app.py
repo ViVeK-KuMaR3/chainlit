7f16bbebb8ec5c85c209033b9e95776e3a5ec5a4
ViVeK-KuMaR3 <v.kumar7643010@gmail.com>
2024-05-04 13:32:53 +0530

    Commit for chnages in app.py

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
 import chainlit as cl
 from src.llm import ask_bot
+
+
+@cl.on_message
+async def main(user_message: cl.Message):
+    
+    # Your custom logic goes here...
+    result=ask_bot(user_message.content)
+    # Send a response back to the user
+    await cl.Message(
+        content=f"Received: {result}",
+    ).send()
