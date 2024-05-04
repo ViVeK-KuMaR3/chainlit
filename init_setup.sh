dc327ac0657a1a30bafdd83972051bc8439939b3
ViVeK-KuMaR3 <v.kumar7643010@gmail.com>
2024-05-04 11:18:44 +0530

    commiting changes for chainlit version

--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,8 @@
 echo [$(date)]: "START"
 echo [$(date)]: "creating a conda env with python 3.9"
-conda create --prefixt ./env python=3.9 -y
+conda create -p ./venv python=3.9 -y
 echo [$(date)]: "created conda env"
-source activate ./env
+source activate ./venv
 echo [$(date)]: "activated conda env"
 echo [$(date)]: "installing requirements"
 pip install -r requirements.txt
---
+++ src/config.py
@@ -0,0 +1,3 @@
+import chainlit
+
+print('chainlit installed')
